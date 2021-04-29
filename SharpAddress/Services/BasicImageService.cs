1108e1a0f1099ac4b1194a7320db8074c0b314b3
sbrevolution5 <sbrevolution5@aim.com>
2021-04-29 14:41:56 -0400

    added imageservice from moviepro project

---
+++ SharpAddress/Services/BasicImageService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MoviePro.Services
+{
+    public class BasicImageService : IImageService
+    {
+        public string DecodeImage(byte[] image, string contentType)
+        {
+            //check if this is null, if so return null
+            if(image == null)
+            {
+                return null;
+            }
+            var imageString = Convert.ToBase64String(image);
+            return $"data:{contentType};base64,{imageString}";
+        }
+
+        public async Task<byte[]> EncodeImageURLAsync(string imageURL)
+        {
+            var client = new HttpClient();
+
+            var response = await client.GetAsync(imageURL);
+            Stream stream = await response.Content.ReadAsStreamAsync();
+            var ms = new MemoryStream();
+            await stream.CopyToAsync(ms);
+            return ms.ToArray();
+        }
+
+        public async Task<byte[]> EncodeImageAsync(IFormFile image)
+        {
+            if(image == null)
+            {
+                return null;
+            }
+            using var ms = new MemoryStream();
+            await image.CopyToAsync(ms);
+            return ms.ToArray();
+
+
+        }
+
+        public string RecordContentType(IFormFile image)
+        {
+            if(image == null)
+            {
+                return null;
+            }
+            return image.ContentType;
+        }
+    }
+}
---
+++ SharpAddress/Services/IImageService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviePro.Services
+{
+    public interface IImageService
+    {
+        /// <summary>
+        /// encode an image from an upload control
+        /// </summary>
+        /// <param name="image">Image from form</param>
+        /// <returns>bytearray of image</returns>
+        Task<byte[]> EncodeImageAsync(IFormFile image);
+
+        //encode an image from a URL
+        Task<byte[]> EncodeImageURLAsync(string imageURL);
+        string DecodeImage(byte[] image, string contentType);
+        string RecordContentType(IFormFile image);
+        
+
+    }
+}
